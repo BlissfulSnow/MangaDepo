adf352d48ed33dc72bc3655035e2b8e07f668af3
BlissfulSnow <carlson.sutanto@gmail.com>
2024-02-06 10:20:12 -0800

    updatevers

--- client/src/pages/Books.jsx
+++ client/src/pages/Books.jsx
@@ -27,28 +27,55 @@ const Books = () => {
         }
     }
 
+    const toggleDescription = (mangaID) => {
+        setBooks(books.map(book => {
+            if (book.mangaID === mangaID) {
+                return { ...book, expanded: !book.expanded };
+            }
+            return book;
+        }));
+    };
+
   return (
     <div>
-        <h1>Manga Titles</h1>
+        <h1 className='title'>Manga Titles</h1>
         <div className="books">
-            {books.map(book => (
-                <div className="book" key={book.mangaID}>
-                    {book.mangaCover && <img src={book.mangaCover} alt="" />}
-                    <h2>{book.mangaTitle}</h2>
-                    <p>{book.mangaDesc}</p>
-                    <button className='delete' onClick={()=>handleDelete(book.mangaID)}>Delete</button>
-                    <button className='update'>Update</button>
-                </div>
+            {books.map((book, index) => (
+                // Create a new row for every 4th book
+                index % 4 === 0 && (
+                    <div className="row" key={`row_${index / 4}`}>
+                        {books.slice(index, index + 4).map(book => (
+                            <div className="book" key={book.mangaID}>
+                                {book.mangaCover && <img src={book.mangaCover} alt="" />}
+                                <h2>{book.mangaTitle}</h2>
+                                
+                                <p>{book.expanded ? book.mangaDesc : `${book.mangaDesc.slice(0, 100)}...`}</p>
+                                {book.mangaDesc.length > 100 && (
+                                <button onClick={() => toggleDescription(book.mangaID)} className='readmorebutton'>
+                                    {book.expanded ? "Show less" : "Read more"}
+                                </button>
+                                )}
+                                <div className='deletebutton'>
+                                    <button className='delete' onClick={()=>handleDelete(book.mangaID)}>Delete</button>
+                                </div>
+                                <div className='updatebutton'>
+                                    <button className='update'>Update</button>
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+                )
             ))}
         </div>
         
-        <div>
-            <button>
-                <Link to="/add">Add New Manga</Link>
+        <div className='addnewmanga'>
+            <button className='button1'>
+                <Link to="/add" className='addtext'>Add New Manga</Link>
             </button>
         </div>
     </div>
-  )
+)
+
 }
 
 export default Books
